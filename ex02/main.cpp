a228da3f6fcb02b4b6194377d0f235056a5437f7
Tom Lasseret <tlassere@k1r1p5.42mulhouse.fr>
2024-06-17 00:48:46 +0200

    Feat: j'ai pas fini j'ai pas les meme resultat

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,38 +6,67 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 20:06:50 by tlassere          #+#    #+#             */
-/*   Updated: 2024/06/17 00:23:38 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/06/17 00:46:33 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stack>
 #include <iostream>
 #include "MutantStack.hpp"
+#include <list>
 #include <deque>
 
 int	main(void)
 {
-	std::cout << "mutant stack" << std::endl;
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << "MutantStack" << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		//mstack.pop();
+		//std::cout << mstack.top() << std::endl;
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+		std::cout << "top: " << mstack.top() << std::endl;
+		std::cout << "size: " << mstack.size() << std::endl;
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
 	}
-	std::stack<int> s(mstack);
+	{
+		std::cout << "List" << std::endl;
+		std::list<int> mstack;
+		mstack.push_back(5);
+		mstack.push_back(17);
+		std::cout << mstack.front() << std::endl;
+		//mstack.pop();
+		//std::cout << mstack.top() << std::endl;
+		std::cout << mstack.size() << std::endl;
+		mstack.push_back(3);
+		mstack.push_back(5);
+		mstack.push_back(737);
+		mstack.push_back(0);
+		std::cout << "top: " << mstack.front() << std::endl;
+		std::cout << "size: " << mstack.size() << std::endl;
+		std::list<int>::iterator it = mstack.begin();
+		std::list<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
 	return (0);
 }
